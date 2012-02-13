d4091124da42c8d2feb0bcc41995675162589562
Anthony Gelibert <anthony.gelibert@me.com>
2012-02-13 16:01:06 +0100

    Add a french translation.

---
+++ lang/fr/settings.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Language file for configuration manager
+ *
+ * @author:     Riccardo "battlehorse" Govoni <battlehorse@gmail.com>
+ * @author:     Anthony Gelibert <anthony.gelibert@me.com>
+ */
+
+$lang['btl_sidebar_position'] = 'Position de la barre latérale' ;
+$lang['btl_sidebar_name'] = 'Nom de la barre latérale' ;
+$lang['btl_action_palette'] = 'Utiliser des actions "palette"';
+$lang['btl_default_page_actions_status'] = 'Status par défaut du menu « Page »';
+$lang['btl_default_wiki_actions_status'] = 'Status par défaut du menu « Wiki »';
+$lang['btl_default_user_actions_status'] = 'Status par défaut du menu « Utilisateur »';
+$lang['btl_default_submit_actions_status'] = 'Status par défaut du menu « Soumettre »';
+$lang['btl_language'] = 'Langue';
+
+$lang['btl_strip_page_actions'] = 'Page';
+$lang['btl_strip_wiki_actions'] = 'Wiki';
+$lang['btl_strip_user_actions'] = 'Utilisateur';
+$lang['btl_strip_submit_actions'] = 'Soumettre';
+
+?>
