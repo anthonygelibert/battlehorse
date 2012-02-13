2ddb6d27340a44431710ba66daf359fdb9ff40a3
Anthony Gelibert <anthony.gelibert@me.com>
2012-02-13 17:09:34 +0100

    Update "Media Manager"

--- mediamanager.php
+++ mediamanager.php
@@ -1,44 +1,46 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
- "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <?php
 /**
- * DokuWiki Default Template
+ * DokuWiki Media Manager Popup
  *
- * This is the template for the media manager popup
- *
- * You should leave the doctype at the very top - It should
- * always be the very first line of a document.
- *
- * @link   http://wiki.splitbrain.org/wiki:tpl:templates
- * @author Andreas Gohr <andi@splitbrain.org>
+ * @author   Andreas Gohr <andi@splitbrain.org>
+ * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
  */
-?>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $conf['lang']?>" lang="<?php echo $conf['lang']?>" dir="ltr">
+// must be run from within DokuWiki
+if (!defined('DOKU_INC')) die();
+
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $conf['lang']?>"
+  lang="<?php echo $conf['lang']?>" dir="<?php echo $lang['direction'] ?>" class="popup">
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>
-    <?php echo hsc($lang['mediaselect'])?>
-    [<?php echo strip_tags($conf['title'])?>]
-  </title>
-  <?php tpl_metaheaders()?>
-  <link rel="shortcut icon" href="<?php echo DOKU_TPL?>images/favicon.ico" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>
+        <?php echo hsc($lang['mediaselect'])?>
+        [<?php echo strip_tags($conf['title'])?>]
+    </title>
+    <?php tpl_metaheaders()?>
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <?php echo tpl_favicon(array('favicon', 'mobile')) ?>
+    <?php _tpl_include('meta.html') ?>
 </head>
 
 <body>
-<div id="media__manager" class="dokuwiki">
-    <div id="media__left">
-        <?php html_msgarea()?>
-        <h1><?php echo hsc($lang['mediaselect'])?></h1>
+    <!--[if IE 6 ]><div id="IE6"><![endif]--><!--[if IE 7 ]><div id="IE7"><![endif]--><!--[if IE 8 ]><div id="IE8"><![endif]-->
+    <div id="media__manager" class="dokuwiki">
+        <?php html_msgarea() ?>
+        <div id="mediamgr__aside"><div class="pad">
+            <h1><?php echo hsc($lang['mediaselect'])?></h1>
 
-        <?php /* keep the id! additional elements are inserted via JS here */?>
-        <div id="media__opts"></div>
+            <?php /* keep the id! additional elements are inserted via JS here */?>
+            <div id="media__opts"></div>
 
-        <?php tpl_mediaTree() ?>
+            <?php tpl_mediaTree() ?>
+        </div></div>
+
+        <div id="mediamgr__content"><div class="pad">
+            <?php tpl_mediaContent() ?>
+        </div></div>
     </div>
-
-    <div id="media__right">
-        <?php tpl_mediaContent() ?>
-    </div>
-</div>
+    <!--[if ( IE 6 | IE 7 | IE 8 ) ]></div><![endif]-->
 </body>
 </html>
